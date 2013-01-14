e09fb3a7ed19f200eac15595ddd54e0d37dc17b1
Michael Bianco <iloveitaly@gmail.com>
2013-01-14 17:30:33 -0500

    Using retinajs's method of retina detection

--- jquery.retina.js
+++ jquery.retina.js
@@ -19,7 +19,13 @@
 			save_size: true
 		}, (typeof settings === "undefined") ? {} : settings)
 
-		if(window.devicePixelRatio >= 2) {
+		// check if retina; method pulled from retinajs
+		var retinaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+						(min--moz-device-pixel-ratio: 1.5),\
+						(-o-min-device-pixel-ratio: 3/2),\
+						(min-resolution: 1.5dppx)";
+
+		if(window.devicePixelRatio > 1 || (window.matchMedia && matchMedia(retinaQuery).matches)) {
 			this.each(function(index, element) {
 				if(!$(element).attr('src')) return;
 
