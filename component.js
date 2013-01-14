8c5cd5cad73002752aeebd969cad30f47927bd5a
Michael Bianco <iloveitaly@gmail.com>
2013-01-14 17:39:16 -0500

    Adding component.js for bower

---
+++ component.js
@@ -0,0 +1,13 @@
+{
+  "name": "jquery.retina",
+  "version": "1.0.0",
+  "main": "./jquery.loadmask.spin.js",
+  "dependencies": {
+    "jquery": "~1.8",
+  },
+  "description": "Selectively replace non-retina images",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/iloveitaly/jQuery-Retina-Display-Plugin.git"
+  }
+}
